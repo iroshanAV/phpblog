6dbb97193864ab2a54a1cedc7ebf5c35444f45b8
Iroshan Vithanage <iroshanav@gmail.com>
2016-11-14 10:21:26 +0530

    Updated the page

--- phpblog/includes/footer.php
+++ phpblog/includes/footer.php
@@ -1,19 +1,11 @@
-
-
-
-
       </div><!-- /.row -->
-
     </div><!-- /.container -->
-
     <footer class="blog-footer">
       <p>MyDiary</p>
       <p>
         <a href="#">Back to top</a>
       </p>
     </footer>
-
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
